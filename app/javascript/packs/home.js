9518d4d80c06f8b4c1ada6b5afdda810bedd0795
Bill Zhang <billzhang345@gmail.com>
2021-01-03 23:37:32 -0500

    Able to nominate

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -1,3 +1,11 @@
+let searchButton = document.getElementById("searchMovieButton");
+searchButton.onclick = () => {
+    searchMovie();
+};
+
+nominationList = [];
+searchList = [];
+
 function searchMovie() {
     let title = document.getElementById("searchMovieText").value
     let oReq = new XMLHttpRequest();
@@ -22,26 +30,61 @@ function changeSearch(movies) {
     listSection.style.visibility = 'visible'
     
     movies = JSON.parse(movies)
-    let searchList = document.getElementById("searchList");
-    searchList.innerHTML = '';
-    movieList = movies.Search
-    console.log(movieList)
+    let searchListSection = document.getElementById("searchList");
+    searchListSection.innerHTML = '';
+    movieList = movies.Search;
     for (i in movieList) {
         let movie = document.createElement("li");
-        let text = document.createTextNode(movieList[i].Title + " (" + movieList[i].Year + ")")
-        movie.className = "font-normal text-xl"
-        movie.appendChild(text);
-        searchList.appendChild(movie);
+        movie.className = "flex py-1 font-normal text-xl";
 
-        let nominateButton = document.createElement("button");
+        let text = document.createElement("p");
+        text.textContent = movieList[i].Title + " (" + movieList[i].Year + ")";
+        text.className = "px-2";
+        
+        let nominateButtonIcon = document.createElement("i");
+        nominateButtonIcon.className = "fas fa-plus pt-1 text-green-400";
+        nominateButtonIcon.onclick = () => {
+            nominateMovie(text.textContent);
+        };
+        movie.appendChild(nominateButtonIcon);
+        movie.appendChild(text);
+        searchListSection.appendChild(movie);
     }
-    console.log(movieList)
 }
 
-$(() => {
-    $('button#searchMovieButton').on(
-        'click', () => {
-            searchMovie();
+function nominateMovie(name) {
+    if (nominationList.length > 5) {
+        console.log("Nomination list exceeds five movies");
+        return;
+    }
+    if (checkNomination(name)) { return; }
+    nominationList.push(name);
+    console.log(nominationList);
+    updateNomination();
+}
+
+function updateNomination() {
+    let nominationListSection = document.getElementById("nominationList");
+    nominationListSection.innerHTML = '';
+    for (i in nominationList) {
+        let movie = document.createElement("li");
+        movie.className = "flex py-1 font-normal text-xl";
+
+        let text = document.createElement("p");
+        text.textContent = nominationList[i];
+        text.className = "px-2";
+
+        movie.appendChild(text);
+        nominationListSection.appendChild(movie);
+    }
+}
+
+function checkNomination(movie) {
+    for (i in nominationList) {
+        if (i === movie) {
+            console.log("Already Nominated");
+            return true;
         }
-    )
-});
+    }
+    return false;
+}
